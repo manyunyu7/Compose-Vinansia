c40ae49938e3899b5526c2776d8db1f4f518be5d
Henry Augusta <henryaugusta4@gmail.com>
2024-02-02 14:33:32 +0700

    Fix Java Version on modules.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,7 @@ android {
 }
 
 dependencies {
-
+    implementation(project(":vin:core"))
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
